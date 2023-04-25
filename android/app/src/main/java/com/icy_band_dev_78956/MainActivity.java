21e97d825fbb27b7bf99c1efa2dd23e08dba557a
Crowdbotics <engineering@crowdbotics.com>
2023-04-25 12:10:44 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/icy_band_dev_78956/MainActivity.java
@@ -0,0 +1,15 @@
+package com.icy_band_dev_78956;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "icy_band_dev_78956";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-band-dev-78956",
+  url: "https://icy-band-dev-78956.botics.co",
+  api: "https://icy-band-dev-78956.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
